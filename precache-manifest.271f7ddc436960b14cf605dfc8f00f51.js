964b027197d50b050ad414c933b3a7de6cab2b60
Vaibhav Nadgonde <itgenesys@gmail.com>
2020-04-07 20:32:27 +0530

    Updates

---
+++ precache-manifest.271f7ddc436960b14cf605dfc8f00f51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0756eca735cc63ebe3e21c92046f9936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90c6f44acdb7328baef4",
+    "url": "/static/css/main.79d1b22d.chunk.css"
+  },
+  {
+    "revision": "55b2c3a815156ac87ddf",
+    "url": "/static/js/2.8d5ca72d.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.8d5ca72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c6f44acdb7328baef4",
+    "url": "/static/js/main.416e81a6.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "50e5b8df1c3cea56673561add5f77251",
+    "url": "/static/media/332-3324201_gofood-logo-png-logo-go-food-vector.50e5b8df.png"
+  },
+  {
+    "revision": "10bf459a56574da37914ce88139163d1",
+    "url": "/static/media/Foods_(cropped) (1).10bf459a.jpg"
+  }
+]);
